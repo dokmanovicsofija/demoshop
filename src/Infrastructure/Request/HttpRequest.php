78a218ef4eab4a762e0ddd8e89fef79ee309e850
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2024-08-16 13:17:58 +0200

    Request and Response.
    ISSUE: MIDU-923

---
+++ src/Infrastructure/Request/HttpRequest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Infrastructure\Request;
+
+use Infrastructure\Utility\GlobalWrapper;
+
+/**
+ * Class HttpRequest
+ *
+ * Represents an HTTP request. It provides methods to retrieve request information such as HTTP method,
+ * URI, query parameters, and POST data.
+ */
+class HttpRequest
+{
+    /**
+     * @var string The HTTP method used for the request (e.g., GET, POST).
+     */
+    private string $method;
+
+    /**
+     * @var string The URI of the request without the query string.
+     */
+    private string $uri;
+
+    /**
+     * @var array An associative array of query parameters from the request URL.
+     */
+    private array $queryParams;
+
+    /**
+     * @var array An associative array of POST data from the request body.
+     */
+    private array $postData;
+
+    /**
+     * HttpRequest constructor.
+     *
+     * Initializes the HttpRequest object by retrieving and setting the HTTP request method,
+     * * URI, query parameters, and POST data. These values are fetched from the global `$_SERVER`,
+     * * `$_GET`, and `$_POST` super globals using the `GlobalWrapper` class.
+     */
+    public function __construct()
+    {
+        $this->method = GlobalWrapper::getInstanceForType('server')->get('REQUEST_METHOD');
+        $this->uri = strtok(GlobalWrapper::getInstanceForType('server')->get('REQUEST_URI'), '?');
+        $this->queryParams = GlobalWrapper::getInstanceForType('get')->getAll() ?? [];
+        $this->postData = GlobalWrapper::getInstanceForType('post')->getAll() ?? [];
+    }
+
+    /**
+     * Gets the HTTP method used for the request.
+     *
+     * @return string The HTTP method (e.g., GET, POST).
+     */
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function getBody(): string
+    {
+        return file_get_contents('php://input');
+    }
+
+    /**
+     * Gets the URI of the request without the query string.
+     *
+     * @return string The request URI.
+     */
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Gets the query parameters from the request URL.
+     *
+     * @return array An associative array of query parameters.
+     */
+    public function getQueryParams(): array
+    {
+        return $this->queryParams;
+    }
+
+    /**
+     * Gets the POST data from the request body.
+     *
+     * @return array An associative array of POST data.
+     */
+    public function getPostData(): array
+    {
+        return $this->postData;
+    }
+
+    /**
+     * Gets a query parameter by key.
+     *
+     * @param string $key The key of the query parameter.
+     * @param mixed|null $default The default value to return if the key is not found.
+     * @return mixed The value of the query parameter or the default value if not found.
+     */
+    public function get(string $key, mixed $default = null): mixed
+    {
+        return $this->queryParams[$key] ?? $default;
+    }
+
+    /**
+     * Gets a POST parameter by key.
+     *
+     * @param string $key The key of the POST parameter.
+     * @param mixed|null $default The default value to return if the key is not found.
+     * @return mixed The value of the POST parameter or the default value if not found.
+     */
+    public function post(string $key, mixed $default = null): mixed
+    {
+        return $this->postData[$key] ?? $default;
+    }
+
+    /**
+     * Gets and decodes the JSON body of the request.
+     *
+     * @return array The parsed JSON body as an associative array.
+     */
+    public function getParsedBody(): array
+    {
+        $body = file_get_contents('php://input');
+        $data = json_decode($body, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            return [];
+        }
+
+        return $data ?? [];
+    }
+}
---
+++ src/Infrastructure/Response/HtmlResponse.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Infrastructure\Response;
+
+use AllowDynamicProperties;
+
+/**
+ * Class HtmlResponse
+ *
+ * Represents a Html response. It provides methods to set the status code, headers, and body of the response
+ * and to send the response to the client.
+ */
+#[AllowDynamicProperties] class HtmlResponse extends AbstractHttpResponse
+{
+    protected string $body;
+
+    public function __construct(int $statusCode = 200, array $headers = [], string $body = '')
+    {
+        parent::__construct($statusCode, $headers);
+        $this->body = $body;
+    }
+
+    /**
+     * Sets the HTTP status code for the response.
+     *
+     * @param int $statusCode The HTTP status code to set.
+     */
+    public function setStatusCode(int $statusCode): void
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    /**
+     * Adds a header to the response.
+     *
+     * @param string $name The name of the header.
+     * @param string $value The value of the header.
+     */
+    public function addHeader(string $name, string $value): void
+    {
+        $this->headers[] = [$name => $value];
+    }
+
+    /**
+     * Sets the body content of the response.
+     *
+     * @param string $body The body content to set.
+     */
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * Creates an HtmlResponse instance from a view file.
+     *
+     * This static method renders a view file using the provided data and returns
+     * an HtmlResponse object with the rendered content. The view file is included
+     * and its output is captured and used as the response body.
+     *
+     * @param string $viewFile The path to the view file to be included.
+     * @param array $data An associative array of data to be made available in the view.
+     * @param int $statusCode The HTTP status code for the response (default is 200).
+     * @param array $headers An associative array of HTTP headers to include in the response (default is an empty array).
+     * @return HtmlResponse An HtmlResponse instance with the rendered view content, status code, and headers.
+     */
+    public static function fromView(string $viewFile, array $data = [], int $statusCode = 200, array $headers = []): HtmlResponse
+    {
+        extract($data);
+        ob_start();
+        include $viewFile;
+        $content = ob_get_clean();
+        return new self($statusCode, $headers, $content);
+    }
+
+    /**
+     * Sends the HTTP response to the client.
+     *
+     * This method sets the HTTP status code, adds the headers, and outputs the body content.
+     */
+    public function send(): void
+    {
+        http_response_code($this->statusCode);
+        foreach ($this->headers as $header => $value) {
+            header("$header: $value");
+        }
+        echo $this->body;
+    }
+}
