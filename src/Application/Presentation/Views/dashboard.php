8c48457f662844739b92bc3d17214ae6e15fb6ae
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2024-08-19 14:34:07 +0200

    Dashboard page.
    ISSUE: MIDU-925

--- src/Application/Presentation/Views/dashboard.php
+++ src/Application/Presentation/Views/dashboard.php
@@ -1,2 +1,23 @@
-<?php
-echo "Dobrodosli na dashboard!";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="/src/Application/Presentation/Public/css/dashboard.css">
+</head>
+<body>
+<div class="container">
+    <aside class="sideMenu">
+        <ul>
+            <li data-section="dashboard" class="active">Dashboard</li>
+            <li data-section="products">Products</li>
+            <li data-section="categories">Product Categories</li>
+        </ul>
+    </aside>
+    <main id="content" class="content">
+    </main>
+</div>
+<script src="/src/Application/Presentation/Public/js/dashboard.js"></script>
+</body>
+</html>
