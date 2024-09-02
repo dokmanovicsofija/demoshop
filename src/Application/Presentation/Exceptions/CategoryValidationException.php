0606b444399438beaaa3dee2114311b71348d001
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2024-09-02 14:09:51 +0200

    Update code for bad request exception.
    ISSUE: MIDU-929

--- src/Application/Presentation/Exceptions/CategoryValidationException.php
+++ src/Application/Presentation/Exceptions/CategoryValidationException.php
@@ -15,9 +15,9 @@ class CategoryValidationException extends Exception
      * Constructor for CategoryValidationException.
      *
      * @param string $message The message that describes the reason for the exception.
-     * @param int $code The exception code (optional).
+     * @param int $code The exception code (default is 400 for a bad request).
      */
-    public function __construct(string $message = "Category validation failed", int $code = 0)
+    public function __construct(string $message = "Category validation failed", int $code = 400)
     {
         parent::__construct($message, $code);
     }
